e9dbad1e1032a3ccf2a1c08ae1d8267626bc61e6
Matheus Schuh <mschuh@kalray.eu>
2021-05-12 14:22:17 +0200

    Updates c_source_export with params export in json format

--- apps/c_codegen_and_graph/c_source_export.py
+++ apps/c_codegen_and_graph/c_source_export.py
@@ -1,13 +1,21 @@
 """
 Script that exports all source code to files
 
-Author: Aurélien POTIN
+Author: Aurelien POTIN
 """
 from pathlib import Path
+import json
+import numpy as np
 
 import tvm
 from tvm import relay
 from tvm.relay import testing
+
+class NDArrayEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, tvm.runtime.ndarray.NDArray):
+            return obj.asnumpy().tolist()
+        return json.JSONEncoder.default(self, obj)
 
 directory_name = "c-source-export-output/"
 Path(directory_name).mkdir(parents=True, exist_ok=True)
@@ -19,7 +27,6 @@ data_shape = (batch_size,) + image_shape
 out_shape = (batch_size, num_class)
 
 # Getting existing relay IR of the neural network
-#mod, params = relay.testing.mobilenet.get_workload(batch_size,)
 mod, params = relay.testing.squeezenet.get_workload(batch_size, image_shape=image_shape, version = '1.1', num_classes=num_class)
 
 target = 'c'
@@ -35,6 +42,11 @@ print("---Writing Relay graph to JSON---")
 with open(directory_name + 'relay_graph.json', 'w') as export_file:
     export_file.write(graph)
 print("Done")
+
+print("---Writing raw params---")
+json_str = json.dumps(params, cls=NDArrayEncoder, indent=4)
+with open(directory_name + 'params.json', 'w') as export_file:
+    export_file.write(json_str)
 
 print("---Writing params---")
 with open(directory_name + 'params.bin', 'wb') as export_file:
